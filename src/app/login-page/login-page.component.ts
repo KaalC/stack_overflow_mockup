fb8e7296aaf60d414d9ee9c293d36ee277a090ab
Kailash Chaudhary <ckailash12ka@gmail.com>
2020-07-18 17:12:13 +0530

    Login Form validation added

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  strTitle: string;
+  strHeading: string;
+  loginForm: FormGroup;
+
+  email() {
+    return this.loginForm.get('email');
+  }
+
+  password() {
+    return this.loginForm.get('password');
+  }
+  
+  constructor( private fb: FormBuilder, private route: Router) { }
 
   ngOnInit(): void {
+    this.strTitle = 'Welcome to Stack overflow Mockup';
+    this.strHeading = 'The place where your query will come to an end';
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-]+)\\.([a-zA-Z]{2,})$')]],
+      password: ['', [Validators.required, Validators.minLength(6)] ],
+      rememberMe: ['']
+    });
+  }
+
+  loginUser() {
+    if ( this.loginForm.valid ) {
+      this.route.navigate(['/fdss']);
+    }
   }
 
 }
